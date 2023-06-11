c0214ab0158a6ec7e26e95793314cc2b5e1767d7
dana-misyakevych <126189652+dana-misyakevych@users.noreply.github.com>
2023-06-11 16:01:21 +0300

    Update custom_keyboards.py

--- keyboards/custom_keyboards.py
+++ keyboards/custom_keyboards.py
@@ -4,11 +4,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def learning_keyboard(question_index):
 
     keyboard = InlineKeyboardMarkup(row_width=2)
-
-    keyboard.insert(InlineKeyboardButton(
-        '🔽', callback_data=f'next-{question_index}')
-    .insert(InlineKeyboardButton(
-        '🔀', callback_data=f'shuffle-{question_index}')
-    ))
+    button1 = InlineKeyboardButton('🔽', callback_data=f'next-{question_index}')
+    button2 = InlineKeyboardButton('🔀', callback_data=f'shuffle-{question_index}')
+    keyboard.insert(button2).insert(button1)
 
     return keyboard
