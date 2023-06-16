0e8c80b76ddec4473356da97ce727d8944c4d806
Oleh Harasymiuk <olegmagot@gmail.com>
2023-06-16 16:09:54 +0300

    add 2022

--- keyboards/custom_keyboards.py
+++ keyboards/custom_keyboards.py
@@ -16,11 +16,13 @@ def years_keyboard():
     keyboard = InlineKeyboardMarkup()
 
     button1 = InlineKeyboardButton('2018', callback_data=f'years-2018')
-    button2 = InlineKeyboardButton('2023', callback_data=f'years-2023')
-    button3 = InlineKeyboardButton('2019', callback_data=f'years-2019')
-    button4 = InlineKeyboardButton('2020', callback_data=f'years-2020')
+    button2 = InlineKeyboardButton('2019', callback_data=f'years-2019')
+    button3 = InlineKeyboardButton('2020', callback_data=f'years-2020')
+    button4 = InlineKeyboardButton('2021', callback_data=f'years-2021')
+    button5 = InlineKeyboardButton('2022', callback_data=f'years-2022')
+    button6 = InlineKeyboardButton('2023', callback_data=f'years-2023')
 
-    keyboard.insert(button2).insert(button1).insert(button3).insert(button4)
+    keyboard.insert(button2).insert(button1).insert(button3).insert(button4).insert(button5).insert(button6)
 
     return keyboard
 
